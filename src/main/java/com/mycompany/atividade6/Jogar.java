50bf1c8591628989874e203cf1c36fdcc66d4455
Paulo Henrique <paulo24041996@gmail.com>
2023-11-12 18:32:17 -0800

    Jogo Pedra, Papel e Tesoura em Java

--- src/main/java/com/mycompany/atividade6/Jogar.java
+++ src/main/java/com/mycompany/atividade6/Jogar.java
@@ -1,50 +1,53 @@
-package com.mycompany.atividade6;
-import java.util.Random;
-import java.util.Scanner;
+package com.mycompany.atividade6; // Pacote Atividade 6
+import java.util.Random; // Importa a Classe Random
+import java.util.Scanner; // Importa a Classe Scanner
 
 abstract class Jogar {
+    // Método Jogar
     public static void jogar(int tentativas){
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int moedas = 0;
-        int escolhaPremio = 0;
+        Scanner scanner = new Scanner(System.in); // Instancia a Classe Scanner
+        Random random = new Random(); // Instancia a Classe Scanner
+        int moedas = 0; // Inicializa o Premio com 0
+        int escolhaPremio = 0; // Inicializa a Escolha do Prêmio com 0
+        // Loop de Repetição que roda de acordo com uma certa quantidade de tentativas
         for (int i = 0; i < tentativas; i++) {
-            System.out.println("\nEscolha: 1 - Pedra, 2 - Papel, 3 - Tesoura");
-            int escolhaUsuario = scanner.nextInt();
-            int escolhaComputador = random.nextInt(3) + 1;
-            System.out.println("Escolha do Computador: " + escolhaComputador);
+            System.out.println("\nEscolha: 1 - Pedra, 2 - Papel, 3 - Tesoura"); // Escolhe Pedra, Papel ou Tesoura
+            int escolhaUsuario = scanner.nextInt(); // Captura a Escolha Anterior do Usuário
+            int escolhaComputador = random.nextInt(2) + 1; // Gera um número aleátorio de 1 a 3
+            System.out.println("Escolha do Computador: " + escolhaComputador); // Printa a Escolha do Computador
             
+            // Lógica do Jogo
             if (escolhaUsuario == escolhaComputador) {
-                System.out.println("Empate! Tente novamente.");
+                System.out.println("Empate! Tente novamente."); // Se a Escolha do Computador for a mesma do usuário, ele classifica como empate
             } else if ((escolhaUsuario == 1 && escolhaComputador == 3) ||
                        (escolhaUsuario == 2 && escolhaComputador == 1) ||
                        (escolhaUsuario == 3 && escolhaComputador == 2)) {
-                System.out.println("Você ganhou! +200 moedas de ouro.");
-                moedas += 200;
+                System.out.println("Você ganhou! +200 moedas de ouro."); // Lógica caso o usuario ganhe
+                moedas += 200; // Adiciona 200 moedas de ouro
             } else {
-                System.out.println("Você perdeu! Tente novamente.");
+                System.out.println("Você perdeu! Tente novamente."); // Senão a vitória é do Computador
             }
-        }System.out.println("\nFim do jogo. Você ganhou um total de " + moedas + " moedas de ouro.");
-        boolean continuar ;
+        }System.out.println("\nFim do jogo. Você ganhou um total de " + moedas + " moedas de ouro."); // Diz a quantidade de Moedas Ganhas
+        boolean continuar ; // Loop Infinito como Verdadeiro
         if(moedas >= 500){
-            continuar = true;
+            continuar = true; // Continua caso as moedas sejam maior ou igual a 500
         } else{
             System.out.println("Você não ganhou nenhum prêmio, mas a prática leva a perfeição, então tente novamente.");
-            continuar = false;
+            continuar = false; // Encerra o jogo caso não seja
         }   while(continuar){
-             System.out.println("Digite 1 para trocar por uma Desert Eagle, 2 para trocar pelo elixir e 3 para escolher o gênio da lâmpada");
-             escolhaPremio = scanner.nextInt();
+             System.out.println("Digite 1 para trocar por uma Desert Eagle, 2 para trocar pelo elixir e 3 para escolher o gênio da lâmpada"); // Prêmio
+             escolhaPremio = scanner.nextInt(); // Captura a Escolha do Premio
             if(moedas >= 500 && escolhaPremio == 1){
-                System.out.println("Agora você é dono de uma Desert Eagle .50AE");
-                continuar = false;
+                System.out.println("Agora você é dono de uma Desert Eagle .50AE"); // Premio 1 - Desert Eagle .50AE
+                continuar = false; // Encerra o Jogo
             } else if(moedas >= 500 && escolhaPremio == 2){
-                System.out.println("Agora você é dono de um Elixir da Imortalidade");
-                continuar = false;
+                System.out.println("Agora você é dono de um Elixir da Imortalidade"); // Premio 2 - Elixir da Imortalidade
+                continuar = false; // Encerra o Jogo
             } else if(moedas >= 500 && escolhaPremio == 3){
                 System.out.println("Agora você recebeu seu pedido do Gênio da Lâmpada e conseguiu o que queria, mas perdeu os outros prêmios anteriores.");
-                continuar = false;
+                continuar = false; // Encerra o Jogo
             }else{
-                System.out.println("Digite um número válido.");
+                System.out.println("Digite um número válido."); // Digita um numero válido e retorna ao loop
             }
         }
     }
--- src/main/java/com/mycompany/atividade6/Main.java
+++ src/main/java/com/mycompany/atividade6/Main.java
@@ -1,24 +1,26 @@
-package com.mycompany.atividade6;
-import java.util.Scanner;
+package com.mycompany.atividade6; // Pacote Atividade 6
+import java.util.Scanner; // Importa o Scanner
 
+// Classe Main e Herança da Classe Jogar
 public class Main extends Jogar{
+    // Método Main
     public static void main(String[] args) {
-        System.out.println("\n----------JOGO PEDRA, PAPEL E TESOURA----------\n");
+        System.out.println("\n----------JOGO PEDRA, PAPEL E TESOURA----------\n"); // Título do Jogo
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nNesse jogo você tem a chance de ganhar até 1000 moedas de ouro, que poderão ser resgatadas para no futuro. E com 500 moedas de ouro você poderá trocar por uma Desert Eagle, poderá trocar por um elixir que te dá a eternidade ou por uma lâmpada a qual o gênio te dará 3 desejos, desde que não seja qualquer um dos exemplos anteriores. Será que você conseguira o que deseja?\nQUE COMEÇEM OS JOGOS!!!\n");
-        System.out.println("Digite 1 para nivel fácil, 2 para nível médio e 3 para nível difícil.");
-        int level = scanner.nextInt();
+        System.out.println("\nNesse jogo você tem a chance de ganhar até 1000 moedas de ouro, que poderão ser resgatadas para no futuro. E com 500 moedas de ouro você poderá trocar por uma Desert Eagle, poderá trocar por um elixir que te dá a eternidade ou por uma lâmpada a qual o gênio te dará 3 desejos, desde que não seja qualquer um dos exemplos anteriores. Será que você conseguira o que deseja?\nQUE COMEÇEM OS JOGOS!!!\n"); // Descrição do Jogo
+        System.out.println("Digite 1 para nivel fácil, 2 para nível médio e 3 para nível difícil."); // Seleciona o Nível Fácil, Médio ou Difícil
+        int level = scanner.nextInt(); // Captura o Nivel
         switch(level){
-            case 1 -> facil();
-            case 2 -> medio();
-            case 3 -> dificil();
-            default -> System.out.println("Digite uma opção válida.");
+            case 1 -> facil(); // Nivel 1 - Fácil
+            case 2 -> medio(); // Nivel 2 - Médio
+            case 3 -> dificil(); // Nivel 3 - Difícil
+            default -> System.out.println("Digite uma opção válida."); // Diz ao usuário inserir um número válido
         }
-    } public static void facil(){
-        jogar(12);
-    } public static void medio(){
-        jogar(8);
-    } public static void dificil(){
-        jogar(3);
+    } public static void facil(){ // Libera Fácil
+        jogar(12); // Seleciona 12 Tentativas
+    } public static void medio(){ // Nível Médio
+        jogar(8); // Libera 8 Tentativas
+    } public static void dificil(){ // Nivel Difícil
+        jogar(3); // Libera 3 Tentativas
     } 
 }
